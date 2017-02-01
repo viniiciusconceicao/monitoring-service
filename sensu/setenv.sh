d7759f53144b97f466c584277b218b0f9127155a
Vinicius <viniicius.conceicao@gmail.com>
2017-02-01 16:00:36 -0600

    Delete setenv.sh
    change setenv place

--- sensu/setenv.sh
+++
@@ -1,5 +0,0 @@
-sudo -- sh -c 'wget -qO - https://raw.githubusercontent.com/antonosmond/subber/master/subber > /usr/bin/subber && chmod +x /usr/bin/subber'
-cp /home/ubuntu/sensu/docker-compose.yml.bk /home/ubuntu/sensu/docker-compose.yml
-export ADDRESS="$(hostname -I |  cut -f1  -d ' ' )"
-export NAME="$(hostname)"
-subber /home/ubuntu/sensu/docker-compose.yml
